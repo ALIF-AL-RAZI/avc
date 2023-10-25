da22b5d981502d87b1e67cbe71be55d0b3be662e
ALIF-AL-RAZI <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2023-10-26 01:14:55 +0600

    Update settings.py

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hyqg203^-bg$p6jautd$(+ilog1g)+-b*c)e_k8cyvgfujbmx*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
